9a73e0299ee83fa50b25e1709f9f2a62c8767b9f
HyeJoonKim <lynn831@naver.com>
2021-02-13 00:05:38 +0900

    Check hidden case

--- DP/boj_13301_타일장식물.py
+++ DP/boj_13301_타일장식물.py
@@ -3,7 +3,7 @@ N = int(input())
 if (N==1): print(4)
 elif (N==2): print(6)
 else:
-    arr = [0] * (N+1)
+    arr = [0] * (N+1)   # hidden case
     arr[1] = 1
     arr[2] = 1
     for i in range(3,N+1):
